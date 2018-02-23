3c2508d094b5c8701e55f1d9df8a4bb98ca4bba6
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-02-23 11:32:18 +0100

    Data types in JavaScript

---
+++ 02dataTypes.js
@@ -0,0 +1,20 @@
+let num = 9;
+console.log(typeof num);
+
+let double = 9.9;
+console.log(typeof double);
+
+let bool = true;
+console.log(typeof bool);
+
+let string = "hi";
+console.log(typeof string);
+
+let emptyString = "";
+console.log(typeof emptyString);
+
+let undefine;
+console.log(typeof undefine);
+
+let whatIsNull = null;
+console.log(typeof whatIsNull);
