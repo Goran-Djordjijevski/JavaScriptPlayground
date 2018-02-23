e6275c3658b28922b90cb948f1f985ea4f74c082
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-02-23 13:50:24 +0100

    Coercion and Conversion in JavaScript

---
+++ 03coercionAndConversion.js
@@ -0,0 +1,13 @@
+let x = 7;
+let y = parseInt("6");
+let z = x + y;
+
+console.log("Anwser: " + z);
+console.log(typeof z);
+
+let nanValue = parseInt("a");
+console.log(nanValue);
+
+var result = isNaN(nanValue);
+console.log(result);
+
