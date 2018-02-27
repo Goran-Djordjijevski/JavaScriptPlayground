158598ab58a2b18448ea5527d7cdf3068e21d145
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-02-27 12:06:55 +0100

    Added new file 04arrays.js.

---
+++ 04arrays.js
@@ -0,0 +1,12 @@
+// Create new array with values
+
+let array = [1, 2, 3, 4, 5];
+
+console.log(array[0]);
+console.log(array[2]);
+console.log(array);
+
+// Undefined because we don't have element at index 5 in the array list
+console.log(array[5]);
+
+console.log(array.length);
