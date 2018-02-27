6adffbb4e2fff34f0ba6349f3abe3280a4bdc450
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-02-27 15:41:20 +0100

    Added new 07decisions.js file.

---
+++ 07decisions.js
@@ -0,0 +1,28 @@
+// if statement
+
+let number = 3;
+if (number == 3) {
+    console.log("This is true")
+}
+else {
+    console.log("This is false");
+}
+
+// switch statement
+let hero = "batman";
+switch (hero.toLowerCase()) {
+    case "batman":
+        console.log("I am batman");
+        break;
+    case "supermen":
+        console.log("I am supermen");
+        break;
+    default:
+        console.log("I am not a superhero");
+        break;
+}
+
+// ternary operator
+let first = 1, second = 1;
+let result = (first == second) ? "equal" : "inequal";
+console.log(result);
