63043d51b3c50d905a96e3e5c69dde87ca268009
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-02-27 14:11:41 +0100

    Create a new file 05functionDeclarations.js file.

---
+++ 05functionDeclarations.js
@@ -0,0 +1,32 @@
+// Create simple function
+function sayHi() {
+    console.log("-----------");
+    console.log("Hi!");
+    console.log("-----------");
+}
+
+// Call the simple function
+sayHi();
+
+// Create a function with the input parameter
+function sayHiWithInputParameter(name) {
+    console.log("-----------");
+    console.log("Hi " + name + "!");
+    console.log("-----------");
+}
+
+// Call a function with a input parameter
+sayHiWithInputParameter("Goran");
+
+// Create a function with a return statement
+function calculateTax(value) {
+    let result = value * 1.1;
+
+    return result;
+}
+
+// Call a function and return a value to the variable
+let tax1 = calculateTax(100);
+let tax2 = calculateTax(200);
+console.log(tax1);
+console.log(tax2);
