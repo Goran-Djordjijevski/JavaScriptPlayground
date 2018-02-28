131aa5ade5325836bcb9b707b19b0888d24707ff
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-02-28 10:22:33 +0100

    Added new javascript 08iterations.js file.

---
+++ 08iterations.js
@@ -0,0 +1,17 @@
+// Simple for loop
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// For loop with array list
+let arr = [4, 8, 15, 16, 23, 42];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+let counter = 0;
+while (counter < arr.length) {
+    console.log(arr[counter]);
+
+    counter++;
+}
