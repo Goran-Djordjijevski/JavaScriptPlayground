fa71080e4555edf8e1e32a746ae6118176acfdb6
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-03-02 12:35:02 +0100

    Added new 16destructuring.js file.

---
+++ 16destructuring.js
@@ -0,0 +1,23 @@
+let a, b, c, d, e;
+
+let names = ["goran", "dejan", "ilija", "dragan", "nemanja"];
+
+[a, b, c, d, e] = names;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+// Destructuring object
+let year, model;
+
+({year, model} = {
+    make: "BMW",
+    model: "745li",
+    year: 2010,
+    value: 5000
+});
+
+console.log(year + " " + model);
