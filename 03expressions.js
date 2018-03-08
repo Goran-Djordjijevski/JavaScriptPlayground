4099a068d2a48614ca335f90a7a0fcf43761d09a
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-03-08 15:37:14 +0100

    Added new 03expressions.js file.

---
+++ 03expressions.js
@@ -0,0 +1,15 @@
+// This is bad
+a;
+
+// Types of expressions
+// ====================
+
+// 1. Variable declarations
+let a;
+
+// 2. Assign a value
+a = 3;
+
+// 3. Perform an evaluation that returns a single value
+// firstExpression + secondExpression
+
