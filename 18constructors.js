a44bed899a5361a0cd5e8bfa8e415eab306f689d
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-05-03 12:27:58 +0200

    Added comments to the 18constructors.js file.

--- 18constructors.js
+++ 18constructors.js
@@ -1,7 +1,9 @@
+// Create a constructor function
 function Car(make, model, year) {
     this.make = make;
     this.model = model;
     this.year = year;
 }
 
+// Create new instance of constructor
 let myCar = new Car("BMW", "745li", 2010);
