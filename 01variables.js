12990be690c0c33f28139a98aba635d60dcd4218
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-09-12 12:10:10 +0200

    Update 01variables.js file.

--- 01variables.js
+++ 01variables.js
@@ -10,3 +10,13 @@ console.log("Answer: " + z);
 // SyntaxError: Identifier 'x' has already been declared
 // let x = 8;
 // console.log(x);
+
+// The rules for naming our variables.
+// Rule 1: All variable names or identifiers have to begin with either a letter, $, or an underscore. 
+// Rule 2: You can't use any reserved keywords.
+
+// Rule 3: Variables names are case sensitive
+let name = "Goran";
+let Name = "Goran";
+let NAME = "Goran"; 
+// These are three different variables
