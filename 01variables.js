b60cc745abe02a7984ab7c2eb01f96da50153f27
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-09-12 11:51:51 +0200

    Update 01variables.js file.

--- 01variables.js
+++ 01variables.js
@@ -2,4 +2,11 @@ let x = 7;
 let y = 3;
 let z = x + y;
 
+// Answer: 10
 console.log("Answer: " + z);
+
+// You can declare variable once but you 
+// can assign its value as many times as you want.
+// SyntaxError: Identifier 'x' has already been declared
+// let x = 8;
+// console.log(x);
