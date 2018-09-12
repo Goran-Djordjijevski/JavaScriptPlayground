5c00bcdf56e9847d7b926268577bc78a7b69c544
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-09-12 12:39:05 +0200

    Added comments to the 02dataTypes.js file.

--- 02dataTypes.js
+++ 02dataTypes.js
@@ -1,20 +1,20 @@
 let num = 9;
-console.log(typeof num);
+console.log(typeof num); // Data type: Number
 
 let double = 9.9;
-console.log(typeof double);
+console.log(typeof double); // Data type: Number
 
 let bool = true;
-console.log(typeof bool);
+console.log(typeof bool); // Data type: Boolean
 
 let string = "hi";
-console.log(typeof string);
+console.log(typeof string); // Data type: String
 
 let emptyString = "";
-console.log(typeof emptyString);
+console.log(typeof emptyString); // Data type: String
 
 let undefine;
-console.log(typeof undefine);
+console.log(typeof undefine); // Data type: Undefined
 
-let whatIsNull = null;
-console.log(typeof whatIsNull);
+let whatIsNull = null; 
+console.log(typeof whatIsNull); // Data type: Object
