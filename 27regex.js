781767d025e961673d35e55be27e6741ecd44a87
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-09-18 11:59:21 +0200

    Add new file for the regular expressoins example.

---
+++ 27regex.js
@@ -0,0 +1,9 @@
+// Regular expressions
+let pattern = /xyz/;
+console.log(pattern); 
+console.log(typeof pattern); // type is object
+
+let value = "This is xyz a test.";
+console.log(pattern.test(value)); // Result is true
+
+console.log(value.replace(pattern, "just"));
